f0e358f819d692f8f1851a0d10cbe130bac5f133
Informatica <informatica@alumno.local>
2024-08-30 14:11:43 -0600

    Separadores de contenedores

--- src/components/GifApp.jsx
+++ src/components/GifApp.jsx
@@ -1,30 +1,17 @@
-import { useState } from "react"
 import { useGif } from "../hooks/useGif"
+import { BuscarGif } from "./BuscarGif"
+import { ContenedorGif } from "./ContenedorGif"
 
 //funtional component
 export const GifApp = () => {
 
-  const [categoria, setCategoria] = useState("")
   const { handleGetGif, arregloGifs } = useGif()
 
-
+  //PROPS = Es una funcion o constante que va al funcional components
   return (
     <>
-
-      <div className="container d-fex flex-row justify-content-center alig-items-center mt-3 w-50">
-        <form className="d-flex" onSubmit={(e) => { handleGetGif(categoria, e) }}>
-          <input className="form-control m-2" type="text" onChange={(e) => { setCategoria(e.target.value) }} />
-          <input className="btn btn-success" type="submit" value="Buscar Gif" />
-        </form>
-      </div>
-
-      <div className="d-flex flex-wrap justify-content-center align-items-center" style={{ gap: '10px' }}>
-        {arregloGifs.map(({ id, url }) => {
-          return (
-            <img className="w-25" key={id} src={url} />
-          )
-        })}
-      </div>
+      <BuscarGif handleGetGif={handleGetGif} />
+      <ContenedorGif arregloGifs={arregloGifs} />
     </>
   )
 }
